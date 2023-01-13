46b80870ad48cd97868dc52cfb574db7cccedc2c
Sowrerajan-sit <sit20it060@sairamtap.edu.in>
2023-01-13 17:06:08 +0530

    piece moving

--- src/Components/Tile/Tile.tsx
+++ src/Components/Tile/Tile.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from "react";
 import "./Tile.css";
 
-interface Props { 
-    number: number;
-    image?: string;
+interface Props {
+  number: number;
+  image?: string;
 }
-function Tile({ number,image }: Props) {
-    if (number % 2 === 0) {
-
-        return (
-            <div className="tile black-tile">
-                <img src={image} alt="" />
-            </div>
-        )
-    } else { 
-        return (
-            <div className="tile white-tile">
-                <img src={image} alt="" />
-
-            </div>
-        )
-        
-    }
+function Tile({ number, image }: Props) {
+  if (number % 2 === 0) {
+    return (
+      <div className="tile black-tile">
+        {image && (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="chess-piece"
+          ></div>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="tile white-tile">
+        {image && (
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="chess-piece"
+          ></div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Tile
+export default Tile;
